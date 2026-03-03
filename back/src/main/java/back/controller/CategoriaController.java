ac8ac535b13815e782166394275ef23cc0b3fd20
Matheus Mendes <matheuskindrow@gmail.com>
2026-03-03 17:12:57 -0300

    add permisao de request em CategoriaController provisorio

--- back/src/main/java/back/controller/CategoriaController.java
+++ back/src/main/java/back/controller/CategoriaController.java
@@ -2,12 +2,14 @@ package back.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import back.model.Categoria;
 import back.service.CategoriaService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/categorias")
 public class CategoriaController {
