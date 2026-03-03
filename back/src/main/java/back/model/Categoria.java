cb802a83424b84d538be9f298d22e35b5dda704f
Matheus Mendes <matheuskindrow@gmail.com>
2026-03-03 15:41:13 -0300

    add restricao unique em categoria (nome)

--- back/src/main/java/back/model/Categoria.java
+++ back/src/main/java/back/model/Categoria.java
@@ -12,7 +12,7 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nome;
 
     public Categoria() {
