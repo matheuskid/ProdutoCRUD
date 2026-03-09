dc37064765e2c73eb5a7898bb9c96fa1d0a5ec13
Matheus Mendes <matheuskindrow@gmail.com>
2026-03-09 15:50:02 -0300

    fix delete categoria

--- back/src/main/java/back/repository/ProdutoRepository.java
+++ back/src/main/java/back/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import back.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
+    boolean existsByCategoriaId(Long categoriaId);
 }
--- back/src/main/java/back/service/CategoriaService.java
+++ back/src/main/java/back/service/CategoriaService.java
@@ -2,51 +2,60 @@ package back.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import back.model.Categoria;
+import back.model.Produto;
 import back.repository.CategoriaRepository;
+import back.repository.ProdutoRepository;
 
 @Service
 public class CategoriaService {
     
-    private final CategoriaRepository repository;
+    private final CategoriaRepository categoriaRepository;
 
-    public CategoriaService(CategoriaRepository repository) {
-        this.repository = repository;
+    private final ProdutoRepository produtoRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository) {
+        this.categoriaRepository = categoriaRepository;
+        this.produtoRepository = produtoRepository;
     }
 
     public List<Categoria> listarCategorias() {
-        return repository.findAll();
+        return categoriaRepository.findAll();
     }
     
     public Categoria criarCategoria(Categoria categoria) {
-        if (repository.existsByNome(categoria.getNome())) {
+        if (categoriaRepository.existsByNome(categoria.getNome())) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Categoria já existe!");   
         }
-        return repository.save(categoria);
+        return categoriaRepository.save(categoria);
     }
 
     public Categoria atualizarCategoria(Long id, Categoria categoria) {
-        if (repository.existsByNome(categoria.getNome())) { //Faz a verificação do nome a ser alterado antes de procurar a categoria, evitando uma consulta desnecessária
+        if (categoriaRepository.existsByNome(categoria.getNome())) { //Faz a verificação do nome a ser alterado antes de procurar a categoria, evitando uma consulta desnecessária
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Categoria já existe!");   
         }
         
-        return repository.findById(id)
+        return categoriaRepository.findById(id)
                 .map(existingCategoria -> {
                     existingCategoria.setNome(categoria.getNome());
-                    return repository.save(existingCategoria);
+                    return categoriaRepository.save(existingCategoria);
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada!"));
     }
 
     public void deletarCategoria(Long id) {
-        if (!repository.existsById(id)) {
+        if (!categoriaRepository.existsById(id)) { // Verifica se a categoria existe
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada!");
         }
-        repository.deleteById(id);
+        if (produtoRepository.existsByCategoriaId(id)) { // Verifica se existem produtos vinculados a essa categoria
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Não é possível deletar esta categoria (produtos vinculados)");
+        }
+        categoriaRepository.deleteById(id);
     }
 
 }
